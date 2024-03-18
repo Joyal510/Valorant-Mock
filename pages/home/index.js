8a2a127bb1d4904ff32c7e2e9098d8eed26951e9
Joyal510 <joyal.corda510@gmail.com>
2024-03-18 19:45:05 +0530

    updated

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,18 +1,18 @@
 import React from "react";
-
-
+import { Suspense } from "react";
 
 import HomeButtons from "@/app/components/HomeButtons";
 
 function Home() {
-  
   return (
     <>
-      <div className="home-page">
-        <div className="nonagon-home"><HomeButtons />
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="home-page">
+          <div className="nonagon-home">
+            <HomeButtons />
+          </div>
         </div>
-        
-      </div>
+      </Suspense>
     </>
   );
 }
