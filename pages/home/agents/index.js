5f6420ce3561eef722810d6311c646969f13f623
Joyal510 <joyal.corda510@gmail.com>
2024-03-18 20:07:59 +0530

    updated

--- pages/home/agents/index.js
+++ pages/home/agents/index.js
@@ -24,29 +24,32 @@ function agents() {
   return (
     <>
       <div className="agents-title">AGENTS</div>
-      <Link href="/home">
-        <div className="back-to-home">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="74"
-            height="74"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-chevron-left"
-          >
-            <path d="m15 18-6-6 6-6" />
-          </svg>
-        </div>
-      </Link>
+
       <div className="agents-components">
         {" "}
         <AgentLeft icons={icons} selectId={selectId} />
         <AgentMiddle icons={icons} onHandleId={onHandleId} />
         <AgentRight icons={icons} selectId={selectId} />
+      </div>
+      <div>
+        <Link href="/home">
+          <div className="back-to-home">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="74"
+              height="74"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-chevron-left"
+            >
+              <path d="m15 18-6-6 6-6" />
+            </svg>
+          </div>
+        </Link>
       </div>
     </>
   );
